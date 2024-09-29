19a10f0c87a3e2e2cfa2f21f84603e84b1e55f4d
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2024-09-29 11:03:57 +0500

    Добавьте файлы проекта.

---
+++ CleanCode&CodeStyle/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanCode_CodeStyle
+{
+    internal class Program
+    {
+        public static int GetNumber(int a, int b, int c)
+        {
+            if (a < b)
+                return b;
+            else if (a > c)
+                return c;
+            else
+                return a;
+        }
+    }
+}
