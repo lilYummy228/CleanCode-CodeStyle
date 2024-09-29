59d7786f1add042faffc875c2aeb9e1662d85d11
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2024-09-29 11:05:40 +0500

    1234

--- CleanCode&CodeStyle/Program.cs
+++ CleanCode&CodeStyle/Program.cs
@@ -1,21 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CleanCode_CodeStyle
+﻿internal class Program
 {
-    internal class Program
+    public static int GetValidNumber(int a, int b, int c)
     {
-        public static int GetValidNumber(int a, int b, int c)
-        {
-            if (a < b)
-                return b;
-            else if (a > c)
-                return c;
-            else
-                return a;
-        }
+        if (a < b)
+            return b;
+        else if (a > c)
+            return c;
+        else
+            return a;
     }
 }
